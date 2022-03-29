ff6158728ce5606a9c9ea880ef24629467300a6e
Jan Goischke <info@inart-designs.de>
2022-03-30 00:59:14 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const { Telegraf } = require('telegraf')
+const express = require('express')
+const expressApp = express()
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+expressApp.use(bot.webhookCallback('/hoox'))
+bot.telegram.setWebhook('https://cryptolinx.net:8443/hoox')
+
+expressApp.get('/', (req, res) => {
+  res.send('Heimdall reconfigured. Pull latest image..... clx/heimdall-v2-edge')
+})
+
+expressApp.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
